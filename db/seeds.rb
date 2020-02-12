c05af0e98df336c99e431bc8218ee71784c1c30c
stephanie sassier <stephanie.sassier@gmail.com>
2020-02-12 14:13:35 +0100

    add seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '05 53 06 17 75',
+    category: 'chinese',
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '05 53 06 17 75',
+    category: 'italian',
+  },
+  {
+    name: 'Le Confidentiel',
+    address: '80 Quai des Chartrons, 33300 Bordeaux',
+    phone_number: '05 57 87 67 72',
+    category: 'french',
+  },
+  {
+    name: 'Matsuri',
+    address: 'Espace Commercial Promenade Ste Catherine, 5 Rue Guiraude, 33000 Bordeaux',
+    phone_number: '09 70 84 51 15',
+    category: 'japanese',
+  },
+  {
+    name: 'Le Petit Bec',
+    address: '14 Rue de la Cour des Aides, 33000 Bordeaux',
+    phone_number: '05 57 83 58 85',
+    category: 'french',
+  }
+]
+Restaurant.create!(restaurants_attributes)
